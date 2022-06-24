0874d3dfa5ad081e46c5627b260914c8e14eb38b
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-06-24 16:55:48 -0600

    WIP building simple data through the REST API to a HAPI-FHIR server

---
+++ 2_encounter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -i \
+  --header "Content-Type: application/fhir+json; charset=UTF-8" \
+  --header "Accept-Charset: utf-8"\
+  --header "User-Agent: HAPI-FHIR/5.0.0 (FHIR Client; FHIR 4.0.1/R4; apache)"\
+  --header "Accept-Encoding: gzip"\
+  --request POST \
+  --data @2_encounter.json\
+  http://localhost:8080/hapi-fhir-jpaserver/fhir/Encounter?_format=json&_pretty=true
+echo $?
---
+++ all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./0_organization.sh
+
+sleep 5
+
+./1_patient.sh
