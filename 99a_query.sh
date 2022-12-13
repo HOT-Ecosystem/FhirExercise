27927d5dbc55281f6846ef16e1980f3c4638d9fd
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-12-13 12:51:26 -0700

    add some queries to help get IDs of new inserts

---
+++ 99a_query.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+url=http://localhost:$PORT/fhir/
+
+curl -i \
+  --header "Content-Type: application/fhir+json; charset=UTF-8" \
+  --header "Accept-Charset: utf-8"\
+  --header "User-Agentm HAPI-FHIR/5.0.0 (FHIR Client; FHIR 4.0.1/R4; apache)"\
+  --output everything.out \
+  --request GET \
+  $url/Organization
+
+#  $url/?_format=json&_pretty=true
