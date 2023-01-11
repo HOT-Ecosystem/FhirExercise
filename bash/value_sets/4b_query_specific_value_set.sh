9fecfe0e48a7d996062310b713b6de67498fe645
Chris Roeder <chris.roeder@cuanschutz.edu>
2023-01-11 10:07:11 -0700

    move value set code and data to it's own directory

--- bash/value_sets/4b_query_specific_value_set.sh
+++ bash/value_sets/4b_query_specific_value_set.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# TODO: only retrieves a single page.
-# this is implemented in JS in the TimsUI, and in python in the TSDemoboard notebook for valuesets
+
 url=http://localhost:$PORT/fhir/
 
 curl -i \
   --header "Content-Type: application/fhir+json; charset=UTF-8" \
   --header "Accept-Charset: utf-8"\
   --header "User-Agent: HAPI-FHIR/5.0.0 (FHIR Client; FHIR 4.0.1/R4; apache)"\
-  --output value_set_all.out \
+  --output value_set_19009.out \
   --request GET \
-  $url/ValueSet
+  $url/ValueSet/19009
 
